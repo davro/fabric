727f7e1a3f57f0591f62143eb9f6dd7af468c700
David Stevens <mail.davro@gmail.com>
2023-09-03 07:51:49 +0100

    inital commmit

---
+++ src/Fabric.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fabric;
+
+use Fabric\Htmx;
+
+/**
+ * Fabric.
+ *
+ */
+class Fabric
+{
+    public static function version(): float
+    {
+        return 0.1;
+    }
+}
