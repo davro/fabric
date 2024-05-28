d510507fca4f1724e951e79833c7b933df8f72f0
David Stevens <mail.davro@gmail.com>
2024-05-28 22:06:06 +0000

    Added composer update and function to retrive FabricationEngine using getEngine in Fabric

--- src/Fabric.php
+++ src/Fabric.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Fabric;
 
 use Fabric\Htmx;
+use Fabrication\FabricationEngine;
 
 /**
  * Fabric Engine.
@@ -15,5 +16,10 @@ class Fabric
     public static function version(): float
     {
         return 0.1;
+    }
+    
+    public function getEngine()
+    {
+        return new FabricationEngine;
     }
 }
